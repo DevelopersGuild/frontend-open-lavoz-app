371f9ad97218297dcb34073e82276820e8ecfcf4
Carlos Alba <soradev4@gmail.com>
2019-12-01 22:50:25 -0800

    change

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,15 +3,20 @@ import queryString from "query-string"
 import { Grommet, grommet, Text } from 'grommet';
 
 function Story({ match, location }) {
+    const HREF = queryString.parse(location.search).href;
+
+    if(!HREF) {
+        return(
+            <Grommet theme={grommet} >
+
+            </Grommet>
+        )
+    }
     return (
         <Grommet theme={grommet} >
             <Text>
-                Story
+                {HREF}
             </Text>
-            <p>
-                <strong>Location Props: </strong>
-                {JSON.stringify(match)}
-            </p>
         </Grommet>
     )
 }
