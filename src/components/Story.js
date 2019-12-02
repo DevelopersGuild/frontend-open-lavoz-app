2964c9a46f00c4db46eef9081fa8bd2e808de94a
Carlos Alba <soradev4@gmail.com>
2019-12-01 23:46:31 -0800

    change

--- src/components/Story.js
+++ src/components/Story.js
@@ -71,7 +71,7 @@ function Story({ match, location }) {
                                 {data.story.headline}
                             </Heading>
                             <Box wrap direction="row">
-                                <Text style={{ padding: "1vw" }} >By: {data.story.by}</Text>
+                                <Text style={{ padding: "1vw" }} >By: {data.story.by || 'Unknown Contributor'}</Text>
                                 <Text style={{ padding: "1vw" }} >Date: {data.story.date}</Text>
                                 <Anchor style={{ padding: "1vw" }} target="__blank" href={data.story.href} label="Original Story" />
                             </Box>
