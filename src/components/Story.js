714329ea413f632abb6848552293176adfb37544
Carlos Alba <soradev4@gmail.com>
2019-12-01 22:41:47 -0800

    change

---
+++ src/components/Story.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grommet, grommet, Text } from 'grommet';
+
+function Story(){
+    return(
+        <Grommet theme={grommet} >
+            <Text>
+                Story
+            </Text>
+        </Grommet>
+    )
+}
+
+export default Story;
