7c1ce0eb9b390bffa15a5a46eef679cc8ca9da2e
Carlos Alba <soradev4@gmail.com>
2019-12-02 00:37:46 -0800

    changes

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,8 @@ import queryString from "query-string";
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { Grommet, grommet, Text, Box, Heading, Image, Anchor, Paragraph } from 'grommet';
-import { AppBar, Toolbar, Typography, Chip } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Chip, Button } from '@material-ui/core';
+import { Facebook, Twitter } from '@material-ui/icons';
 
 const GET_STORY = (href) => gql`
 {
@@ -74,6 +75,12 @@ function Story({ match, location }) {
                                 <Text style={{ padding: "1vw" }} >By: {data.story.by.trim().length > 0 ? data.story.by : 'Unknown Contributor'}</Text>
                                 <Text style={{ padding: "1vw" }} >Date: {data.story.date}</Text>
                                 <Anchor style={{ padding: "1vw" }} target="__blank" href={data.story.href} label="Original Story" />
+                                <Button size="small" style={{ margin: "1vw" }} startIcon={<Facebook />} variant="contained" color="default" onClick={() => window.open(`https://www.facebook.com/sharer/sharer.php?u=${HREF}`)}>
+                                    Share on Facebook
+                                </Button>
+                                <Button size="small" style={{ margin: "1vw" }} startIcon={<Twitter />} variant="contained" color="default" onClick={() => window.open(`https://twitter.com/intent/tweet?text=${data.story.headline}${" via Open La Voz"}&url=${HREF}`)}>
+                                    Share on Twitter
+                                </Button>
                             </Box>
                             <Box pad="large" align="center" justify="center" >
                                 <Paragraph fill size="medium" margin="none" textAlign="center">
@@ -81,7 +88,7 @@ function Story({ match, location }) {
                                 </Paragraph>
                             </Box>
                             <Box wrap direction="row">
-                                {data.story.tags.map((x, key) => <Chip onClick={() => { window.open(`https://lavozdeanza.com/tag/${x}`, '__blank') }} clickable style={{ marginRight: "0.5vw", marginLeft: "0.5vw" }} key={key} label={x} />)}
+                                {data.story.tags.map((x, key) => <Chip onClick={() => { window.open(`https://lavozdeanza.com/tag/${x.replace(/\s+/g, '-').toLowerCase()}`, '__blank') }} clickable style={{ marginRight: "0.5vw", marginLeft: "0.5vw" }} key={key} label={x} />)}
                             </Box>
                         </Box>
                     )
