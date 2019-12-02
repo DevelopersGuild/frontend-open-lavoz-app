b12d5433cb5aebbc0e241dcce49d423511d35953
Carlos Alba <soradev4@gmail.com>
2019-12-01 22:42:19 -0800

    change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Listings from './components/Listings';
+import Story from './components/Story';
 
 function App() {
   return(
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Listings} />
+        <Route exact path="/story" component={Story} />
       </Switch>
     </BrowserRouter>
   );
